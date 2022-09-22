4bed745bff8b68f507b6e885f9a3b10513bed612
KasperNissen1997 <KasperNissen1997@gmail.com>
2022-09-22 13:43:45 +0200

    Added mail field.

--- Guest.cs
+++ Guest.cs
@@ -2,11 +2,13 @@
 {
     internal class Guest
     {
+        string mail;
         string name;
         string firm;
 
-        public Guest (string name, string firm)
+        public Guest (string mail, string name, string firm)
         {
+            this.mail = mail;
             this.name = name;
             this.firm = firm;
         }
